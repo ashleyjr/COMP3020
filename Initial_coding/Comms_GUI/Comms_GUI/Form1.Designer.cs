b91f95b19a7c306b22bced89313e1fff9b6ce03c
ashleyjr <ashley181291@gmail.com>
2012-09-09 23:48:51 +0100

    Added graphs with dummy data inputs to comms_GUI

--- Initial_coding/Comms_GUI/Comms_GUI/Form1.Designer.cs
+++ Initial_coding/Comms_GUI/Comms_GUI/Form1.Designer.cs
@@ -29,11 +29,164 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Time_XY_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.XY_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Step_timer = new System.Windows.Forms.Timer(this.components);
+            this.Yaxis_trackBar = new System.Windows.Forms.TrackBar();
+            this.Xaxis_trackBar = new System.Windows.Forms.TrackBar();
+            this.Xaxis_label = new System.Windows.Forms.Label();
+            this.Yaxis_label = new System.Windows.Forms.Label();
+            this.GO_button = new System.Windows.Forms.Button();
+            this.Timer_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Time_XY_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XY_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Yaxis_trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Xaxis_trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Time_XY_chart
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.Time_XY_chart.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.Time_XY_chart.Legends.Add(legend7);
+            this.Time_XY_chart.Location = new System.Drawing.Point(12, 12);
+            this.Time_XY_chart.Name = "Time_XY_chart";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series10.Legend = "Legend1";
+            series10.Name = "Xaxis_Angle";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series11.Legend = "Legend1";
+            series11.Name = "Yaxis_Angle";
+            this.Time_XY_chart.Series.Add(series10);
+            this.Time_XY_chart.Series.Add(series11);
+            this.Time_XY_chart.Size = new System.Drawing.Size(450, 300);
+            this.Time_XY_chart.TabIndex = 0;
+            this.Time_XY_chart.Text = "Time_XY";
+            // 
+            // XY_chart
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.XY_chart.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.XY_chart.Legends.Add(legend8);
+            this.XY_chart.Location = new System.Drawing.Point(468, 12);
+            this.XY_chart.Name = "XY_chart";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series12.Legend = "Legend1";
+            series12.Name = "X_Vs_Y_Angle";
+            this.XY_chart.Series.Add(series12);
+            this.XY_chart.Size = new System.Drawing.Size(450, 300);
+            this.XY_chart.TabIndex = 1;
+            this.XY_chart.Text = "XY_chart";
+            // 
+            // Step_timer
+            // 
+            this.Step_timer.Tick += new System.EventHandler(this.Step_timer_Tick);
+            // 
+            // Yaxis_trackBar
+            // 
+            this.Yaxis_trackBar.Location = new System.Drawing.Point(612, 318);
+            this.Yaxis_trackBar.Maximum = 90;
+            this.Yaxis_trackBar.Minimum = -90;
+            this.Yaxis_trackBar.Name = "Yaxis_trackBar";
+            this.Yaxis_trackBar.Size = new System.Drawing.Size(150, 45);
+            this.Yaxis_trackBar.TabIndex = 2;
+            this.Yaxis_trackBar.Scroll += new System.EventHandler(this.Yaxis_trackBar_Scroll);
+            // 
+            // Xaxis_trackBar
+            // 
+            this.Xaxis_trackBar.Location = new System.Drawing.Point(768, 318);
+            this.Xaxis_trackBar.Maximum = 90;
+            this.Xaxis_trackBar.Minimum = -90;
+            this.Xaxis_trackBar.Name = "Xaxis_trackBar";
+            this.Xaxis_trackBar.Size = new System.Drawing.Size(150, 45);
+            this.Xaxis_trackBar.TabIndex = 3;
+            this.Xaxis_trackBar.Scroll += new System.EventHandler(this.Xaxis_trackBar_Scroll);
+            // 
+            // Xaxis_label
+            // 
+            this.Xaxis_label.AutoSize = true;
+            this.Xaxis_label.Location = new System.Drawing.Point(768, 350);
+            this.Xaxis_label.Name = "Xaxis_label";
+            this.Xaxis_label.Size = new System.Drawing.Size(32, 13);
+            this.Xaxis_label.TabIndex = 4;
+            this.Xaxis_label.Text = "Xaxis";
+            // 
+            // Yaxis_label
+            // 
+            this.Yaxis_label.AutoSize = true;
+            this.Yaxis_label.Location = new System.Drawing.Point(609, 350);
+            this.Yaxis_label.Name = "Yaxis_label";
+            this.Yaxis_label.Size = new System.Drawing.Size(32, 13);
+            this.Yaxis_label.TabIndex = 5;
+            this.Yaxis_label.Text = "Yaxis";
+            // 
+            // GO_button
+            // 
+            this.GO_button.Location = new System.Drawing.Point(12, 318);
+            this.GO_button.Name = "GO_button";
+            this.GO_button.Size = new System.Drawing.Size(75, 23);
+            this.GO_button.TabIndex = 6;
+            this.GO_button.Text = "GO";
+            this.GO_button.UseVisualStyleBackColor = true;
+            this.GO_button.Click += new System.EventHandler(this.GO_button_Click);
+            // 
+            // Timer_label
+            // 
+            this.Timer_label.AutoSize = true;
+            this.Timer_label.Location = new System.Drawing.Point(94, 319);
+            this.Timer_label.Name = "Timer_label";
+            this.Timer_label.Size = new System.Drawing.Size(35, 13);
+            this.Timer_label.TabIndex = 7;
+            this.Timer_label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(932, 388);
+            this.Controls.Add(this.Timer_label);
+            this.Controls.Add(this.GO_button);
+            this.Controls.Add(this.Yaxis_label);
+            this.Controls.Add(this.Xaxis_label);
+            this.Controls.Add(this.Xaxis_trackBar);
+            this.Controls.Add(this.Yaxis_trackBar);
+            this.Controls.Add(this.XY_chart);
+            this.Controls.Add(this.Time_XY_chart);
+            this.Name = "Form1";
+            this.Text = "Comms_GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Time_XY_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XY_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Yaxis_trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Xaxis_trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Time_XY_chart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart XY_chart;
+        private System.Windows.Forms.Timer Step_timer;
+        private System.Windows.Forms.TrackBar Yaxis_trackBar;
+        private System.Windows.Forms.TrackBar Xaxis_trackBar;
+        private System.Windows.Forms.Label Xaxis_label;
+        private System.Windows.Forms.Label Yaxis_label;
+        private System.Windows.Forms.Button GO_button;
+        private System.Windows.Forms.Label Timer_label;
     }
 }
 
--- Initial_coding/Comms_GUI/Comms_GUI/Form1.cs
+++ Initial_coding/Comms_GUI/Comms_GUI/Form1.cs
@@ -11,9 +11,125 @@ namespace Comms_GUI
 {
     public partial class Form1 : Form
     {
+        private double total_counter;
+
+        private double Xaxis;
+        private double Yaxis;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Variable setup
+            total_counter = 0;
+            Xaxis = 0;
+            Yaxis = 0;
+            //Chart setup
+            XY_chart.Series["X_Vs_Y_Angle"].BorderWidth = 4;
+            XY_chart.ChartAreas[0].AxisX.Maximum = 90;
+            XY_chart.ChartAreas[0].AxisX.Minimum = -90;
+            XY_chart.ChartAreas[0].AxisY.Maximum = 90;
+            XY_chart.ChartAreas[0].AxisY.Minimum = -90;
+            XY_chart.Series["X_Vs_Y_Angle"].Points.AddXY(0, 0);
+
+            Time_XY_chart.Series["Xaxis_Angle"].BorderWidth = 4;
+            Time_XY_chart.Series["Yaxis_Angle"].BorderWidth = 4;
+
+            //The rest
+            Step_timer.Enabled = false;
+            Timer_label.Text = "Timer elapsed:";
+        }
+
+        private void Step_timer_Tick(object sender, EventArgs e)
+        {
+            total_counter += 0.1;
+            Update_time_label();
+            Time_XY_chart.Series["Xaxis_Angle"].Points.AddXY(total_counter, Xaxis);
+            Time_XY_chart.Series["Yaxis_Angle"].Points.AddXY(total_counter, Yaxis);
+
+            XY_chart.Series["X_Vs_Y_Angle"].Points.Clear();
+            XY_chart.Series["X_Vs_Y_Angle"].Points.AddXY(Xaxis, Yaxis);
+        }
+
+        //Converts to mins and seconds after 60 seconds
+        private void Update_time_label()
+        {
+            string num_s = "";
+            string num_m = "";
+            string num_h = "";
+
+            if ((total_counter / 3600) > 1)
+            {
+                num_h = (total_counter / 3600).ToString();
+                if (num_h.Contains("."))
+                {
+                    num_h = num_h.Split('.')[0];
+                }
+                num_m = ((total_counter - (Convert.ToInt32(num_h) * 3600)) / 60).ToString();
+                if (num_m.Contains("."))
+                {
+                    num_m = num_m.Split('.')[0];
+                }
+                num_s = (total_counter - (Convert.ToInt32(num_h) * 3600) - (Convert.ToInt32(num_m) * 60)).ToString();
+                if (num_s.Contains("."))
+                {
+                    num_s = num_s.Split('.')[0];
+                }
+                Timer_label.Text = "Time elapsed: " + num_h + "h" + num_m + "m" + num_s + "s";
+            }
+            else
+            {
+                if ((total_counter / 60) > 1)
+                {
+                    num_m = (total_counter / 60).ToString();
+                    if (num_m.Contains("."))
+                    {
+                        num_m = num_m.Split('.')[0];
+                    }
+                    num_s = (total_counter - (Convert.ToInt32(num_m) * 60)).ToString();
+                    if (num_s.Contains("."))
+                    {
+                        num_s = num_s.Split('.')[0];
+                    }
+                    Timer_label.Text = "Time elapsed: " + num_m + "m" + num_s + "s";
+                }
+                else
+                {
+                    num_s = total_counter.ToString();
+                    if (num_s.Contains("."))
+                    {
+                        num_s = num_s.Split('.')[0];
+                    }
+                    Timer_label.Text = "Time elapsed: " + num_s + "s";
+                }
+            }
+        }
+
+        private void GO_button_Click(object sender, EventArgs e)
+        {
+            if (GO_button.Text == "GO")
+            {
+                GO_button.Text = "STOP";
+                Step_timer.Enabled = true;
+            }
+            else
+            {
+                GO_button.Text = "GO";
+                Step_timer.Enabled = false;
+            }
+        }
+
+        private void Yaxis_trackBar_Scroll(object sender, EventArgs e)
+        {
+            Yaxis = Yaxis_trackBar.Value;
+        }
+
+        private void Xaxis_trackBar_Scroll(object sender, EventArgs e)
+        {
+            Xaxis = Xaxis_trackBar.Value;
         }
     }
 }
